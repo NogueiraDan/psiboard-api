75918318fe651ed8780f73a3a7d9c7a1ff2620ac
NogueiraDan <silvanogueira3460@gmail.com>
2024-03-21 08:39:38 -0300

    feat: Setup banco de dados de produção

--- src/config/ormconfig.ts
+++ src/config/ormconfig.ts
@@ -5,10 +5,26 @@ import { Patient } from 'src/patient/entities/patient.entity';
 import { Scheduling } from 'src/scheduling/entities/scheduling.entity';
 
 //CONFIG DO SQLITE - Esse Banco é util para fins de desenvolvimento.
+// export const config: DataSourceOptions = {
+//   type: 'sqlite',
+//   database: './database/database.sqlite',
+//   synchronize: true,
+//   logging: true,
+//   entities: [Professional, Patient, Scheduling],
+// };
+
+// CONFIG NEON POSTGRESQL - Banco de produção da aplicação.
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
 export const config: DataSourceOptions = {
-  type: 'sqlite',
-  database: './database/database.sqlite',
+  type: 'postgres',
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: true,
   synchronize: true,
   logging: true,
   entities: [Professional, Patient, Scheduling],
 };
+
